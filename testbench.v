5480475d612e8d718a5a58a35526fd6e0ff81563
Ace <133bha@gmail.com>
2017-03-31 17:33:20 +0700

    Add files via upload

--- testbench.v
+++ testbench.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns/100ps
 
 
@@ -12,8 +13,7 @@ module test_bndflsh ;
     wire [MX_LP-1:0] lamp ;
 
     // connect signals to flasher
-    bound_flasher bound_flasher_01 (.clk(clk), .rst_n(rst_n), .flick(flick),.a_lamp(lamp),
-            .a_next_state(next_state)  ) ;
+    bound_flasher bound_flasher_01 (.clk(clk), .rst_n(rst_n), .flick(flick),.lamp(lamp)) ;
 
     always begin // clock generator
         clk = 1'b1 ;
@@ -21,114 +21,70 @@ module test_bndflsh ;
         # HALF_CYCLE ;
     end
 
-    always @ ( posedge clk ) $strobe ("t= %d, rst_n=%b, clk=%b, flick=%b, next_st=%b, lamp=%b",
-                $stime, rst_n, clk, flick,next_state, lamp ) ;
+    always @ ( posedge clk ) $strobe ("t= %d, rst_n=%b, clk=%b, flick=%b,lamp=%b",
+                $stime, rst_n, clk, flick, lamp ) ;
 
     initial begin
-            rst_n = 1'b1;
-       # CYCLE rst_n = 0 ;
-        # (CYCLE * 3 ) rst_n = 1'b1 ;
+//        rst_n = 1'b1;
+//        #CYCLE rst_n = 0 ;
+//        #(CYCLE * 3 ) rst_n = 1'b1 ;
+        
+                    rst_n = 1'b0;
+        #(CYCLE*2)  rst_n = 1'b1;
+        #(CYCLE*157)rst_n = 1'b0;
+        #(CYCLE*4)  rst_n = 1'b1;
+        #(CYCLE*45) rst_n = 1'b0;
+        #(CYCLE*3)  rst_n = 1'b1;
     end
     
     initial begin // give value to control variable
-        flick = 1'b0 ;
+//        flick = 1'b0 ;
+//
+//        # (CYCLE * 4 ) flick = 1'b1 ;
+//        # (CYCLE * 2 ) flick = 1'b0 ;
+//        # (CYCLE * 56 ) flick = 1'b1 ;
+//        # (CYCLE * 2 ) flick = 1'b0 ;
+//
+//        #(CYCLE * 1000 ) $finish; 
 
-        # (CYCLE * 4 ) flick = 1'b1 ;
-        # (CYCLE * 2 ) flick = 1'b0 ;
-        #(CYCLE * 100 ) $finish; 
+                       flick = 1'b0 ;
+        # (CYCLE * 2);
+        # (CYCLE * 2)  flick = 1'b1; 
+        # (CYCLE * 2)  flick = 1'b0;
+        # (CYCLE * 60) flick = 1'b1; 
+        # (CYCLE * 30) flick = 1'b0;
+        # (CYCLE * 50) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 32) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 30) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0;
+        # (CYCLE * 37) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 35) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 32) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0;
+        # (CYCLE * 12) flick = 1'b1; 
+        # (CYCLE * 35) flick = 1'b1; 
+        # (CYCLE * 20) rst_n = 1'b0;
+        # (CYCLE * 5 ) flick = 1'b0;
+        # (CYCLE * 15) rst_n = 1'b1;
+        # (CYCLE * 5 ) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 20) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 29) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+        # (CYCLE * 12) flick = 1'b1; 
+        # (CYCLE * 5)  flick = 1'b0; 
+
+        # (CYCLE * 100) $finish;
+
+
+
     end
 	
-	
-	initial begin
-//-------------------(1) normal case-------------------------------------------------------
-//		#4 rst_n = 1'b1;
-//		#HALF_CYCLE rst_n = 1'b0 ;
-//		#HALF_CYCLE rst_n = 1'b1;
-
-//-------------------(2.1) reset for up_0_15------------------------------------------------
-		#4 rst_n = 1'b1;
-        #HALF_CYCLE rst_n = 1'b0 ;
-        #HALF_CYCLE rst_n = 1'b1;
-		#115	    rst_n = 1'b0;
-		#HALF_CYCLE rst_n = 1'b1;
-
-/*//-------------------(2.2) reset for dow_15_5------------------------------------------------
-        #4 rst_n = 1'b1;
-        #HALF_CYCLE rst_n = 1'b0 ;
-        #HALF_CYCLE rst_n = 1'b1;
-        #215        rst_n = 1'b0;
-        #HALF_CYCLE rst_n = 1'b1;*/
-
-/*//-------------------(2.3) reset for up_5_10------------------------------------------------
-        #4 rst_n = 1'b1;
-        #HALF_CYCLE rst_n = 1'b0 ;
-        #HALF_CYCLE rst_n = 1'b1;
-        #315        rst_n = 1'b0;
-        #HALF_CYCLE rst_n = 1'b1;*/
-
-/*//-------------------(2.4) reset for dow_10_0------------------------------------------------
-        #4 rst_n = 1'b1;
-        #HALF_CYCLE rst_n = 1'b0 ;
-        #HALF_CYCLE rst_n = 1'b1;
-        #395        rst_n = 1'b0;
-        #HALF_CYCLE rst_n = 1'b1;*/
-
-/*//-------------------(2.5) reset for up_0_5------------------------------------------------
-        #4 rst_n = 1'b1;
-        #HALF_CYCLE rst_n = 1'b0 ;
-        #HALF_CYCLE rst_n = 1'b1;
-        #495        rst_n = 1'b0;
-        #HALF_CYCLE rst_n = 1'b1;*/
-
-/*//-------------------(2.6) reset for dow_5_0------------------------------------------------
-        #4 rst_n = 1'b1;
-        #HALF_CYCLE rst_n = 1'b0 ;
-        #HALF_CYCLE rst_n = 1'b1;
-        #545        rst_n = 1'b0;
-        #HALF_CYCLE rst_n = 1'b1;*/
-
-/*//--------------------(4)combination--------------------------------------------------------
-		##4 rst_n = 1'b1;
-        #(HALF_CYCLE*3.5) rst_n = 1'b0 ;
-        #(HALF_CYCLE*2.5) rst_n = 1'b1;
-        #(HALF_CYCLE*7.5) rst_n = 1'b0;
-        #(HALF_CYCLE*4.5) rst_n = 1'b1;
-		#(HALF_CYCLE*5.5) rst_n = 1'b0;
-		#(HALF_CYCLE*0.5) rst_n = 1'b1;*/
-
-	end
-	
-	initial begin
-/*//-------------------(1) normal case-------------------------------------------------------
-		#(HALF_CYCLE*3) flick = 1'b1;
-		#HALF_CYCLE flick= 1'b0;
-		#(CYCLE *100) $finish;*/
-
-//---------------------(3.1) flick = 1 for case DOW_15_5-----------------------------------
-		#(HALF_CYCLE*3) flick = 1'b1;
-        #(CYCLE *100) $finish;
-
-/*//---------------------(3.2) flick = 1 for case DOW_10_5 kickback point lam[5]--------------
-		#(HALF_CYCLE*3) flick = 1'b1;
-        #HALF_CYCLE flick= 1'b0;
-		#345 flick = 1'b1;
-        #(CYCLE *100) $finish;*/
-
-/*//---------------------(3.3) flick = 1 for case DOW_10_5 kickback point lam[0]-----------------
-        #(HALF_CYCLE*3) flick = 1'b1;
-        #HALF_CYCLE flick= 1'b0;
-        #415 flick = 1'b1;
-        #(CYCLE *100) $finish;*/
-
-/*//---------------------(4) combination-----------------------------------------------------
-		#(HALF_CYCLE*3) flick = 1'b1;
-        #HALF_CYCLE flick= 1'b0;
-        #(CYCLE*2.5) flick = 1'b1;
-		#(CYCLE*6.5) flick = 1'b0;
-		#(HALF*11.4) flick = 1'b1;
-		#(CYCLE*50) $finish;*/
-
-	end
 	/*initial begin
 		$vcdplusfile ("bound_flasher.vpd");
 		$vcdpluson ();
@@ -136,6 +92,4 @@ module test_bndflsh ;
 
 endmodule
 
-`include "dut.v"
-
-
+`include "DUT.v"
