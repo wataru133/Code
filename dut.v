ba53abf7108dfa868732e24c35afeeca6060eb0b
Ace <133bha@gmail.com>
2017-04-10 18:27:12 +0700

    Add files via upload

--- dut.v
+++ dut.v
@@ -1,91 +1,100 @@
 //-----------------------------------------------------------------
-//// Project Name : Exercise 2
-//// : LFU
-//// File Name : dut.v
-//// Module Name : lfu
-//// Function : Finds out the Least Frequently Used entry in the four
-//// entries.
-//// Note : 
-//// Author : An Bui (HW1906)
-////------------------------------------------------------------------
-//// History
-//// Version Date Author Description
-////---------------------------------------------------------------
+// Project Name : Rensas RTL coding training, DesignWS_P1
+// 
+// File Name : dut.v
+// Module Name : lfu
+// Function : finds out the Least Frequently Used entry in the four
+// Author : QuangNguyen
+// Version:0
+//-----------------------------------------------------------------
+module lfu ( clk, rst_n, new_buf_req, ref_buf_numbr, buf_num_replc);
 
-module lfu_finder ( clk, rst_n, new_buf_req,
-        ref_buf_numbr, buf_num_replc ) ;
+//-----INPUT SIGNALS-----
+input clk;
+input rst_n;
+input new_buf_req;
+input [1:0]ref_buf_numbr;
+wire clk;
+wire rst_n;
+wire new_buf_req;
+wire [1:0]ref_buf_numbr;
 
-    input clk;
-    input rst_n;
-    input new_buf_req ;
-    input [1:0] ref_buf_numbr ;
+//-----OUTPUT SIGNALS-----
+output [1:0]buf_num_replc;
+wire [1:0]buf_num_replc;
 
-    output[1:0] buf_num_replc ;
+//-----EXTERNAL SIGNALS-----
+reg [7:0]acc_ti;
+reg [1:0]next_buf_num_replc;
+reg [1:0]f_buf_num_replc;
 
-    wire clk; 
-    wire rst_n ;
-    wire new_buf_req ;
-    wire [1:0] ref_buf_numbr ;
-    wire [1:0] buf_num_replc ;
+//-----FF FOR BUF_NUM_REPLC-----
+assign buf_num_replc = f_buf_num_replc;
+always @ (posedge clk or negedge rst_n) begin
+  if (rst_n = 1'b0) begin
+    f_buf_num_repcl <= 2'b00;
+  end
+    f_buf_num_replc <= next_buf_num_replc;
+  else begin
+  end
+end
 
-    // internal signals
-    reg [7:0] ref_seq ; // FF
-    reg [7:0] next_ref_seq ; // non-FF
-
-    reg [7:0] num_acc;
-    reg [7:0] next_num_acc;
-
-    wire [1:0] ref_numbr ;
-
-    assign buf_num_replc = ref_seq[1:0] ;
-    assign ref_numbr = ( new_buf_req == 1'b1 )?
-            ref_seq[7:6] : ref_buf_numbr ;
-
-    always @ ( posedge clk or negedge rst_n ) begin
-        if ( rst_n == 1'b0 ) begin
-            // initialize reference sequence
-            // #0(old), #1, #2, and #3(new) order
-            ref_seq <= 8'b11_10_01_00 ;
-        end
-        else begin
-            ref_seq <= next_ref_seq ;
-        end
-    end
-    
-    always @ ( posedge clk or negedge rst_n ) begin
-        if ( rst_n == 1'b0 ) begin
-            num_acc <= 8'b01_01_01_01 ;
-        end
-        else begin
-            num_acc <= next_num_acc ;
-        end
-    end
-
-    always @ ( ref_seq[7:0] or ref_numbr ) begin
-        case ( ref_numbr[1:0] )
-
-            ref_seq[7:6] : begin
-                new_ref_seq = { ref_seq[5:0], ref_seq[7:6] } ;
-            end
-
-            ref_seq[5:4] : begin
-                new_ref_seq = { ref_seq[7:6], ref_seq[3:0],
-                    ref_seq[5:4] } ;
-            end
-
-            ref_seq[3:2] : begin
-                new_ref_seq = { ref_seq[7:4], ref_seq[1:0],
-                    ref_seq[3:2] } ;
-            end
-
-            ref_ref_seq[1:0] : begin
-                new_ref_seq = { ref_seq[7:2], ref_seq[1:0] } ;
-            end
-
-            default : begin
-                new_ref_seq = 8'bxx_xx_xx_xx ;
-            end
-
-        endcase
-    end
+//-----COUTERN ACCESS TIME-----
+always @ (posedge new_buf_req or ref_buf_numbr) begin
+  if (new_buf_req) begin
+  end
+  else begin
+    case(ref_buf_numbr)
+      2'b00:begin
+          acc_ti[1:0] = (acc_ti[1:0] == 2'b11) ? 2'b11 : (acc_ti[1:0] + 2'b01);
+//        if (acc_ti[1:0] == 2'b11) begin
+//          acc_ti[1:0] = 2'b11;
+//        end
+//        else begin
+//          acc_ti[1:0] = acc_ti[1:0] + 2'b01;
+//        end
+      end
+      2'b01:begin
+          acc_ti[3:2] = (acc_ti[3:2] == 2'b11) ? 2'b11: (acc_ti[3:2] + 2'b01);
+//        if (acc_ti[3:2] == 2'b11) begin
+//          acc_ti[3:2] = 2'b11;
+//        end
+//        else begin
+//          acc_ti[3:2] = acc_ti[3:2] + 2'b01;
+//        end
+      end
+      2'b10:begin
+          acc_ti[5:4] = (acc_ti[5:4] == 2'b11) ? 2'b11: (acc_ti[5:4] + 2'b01);
+//        if (acc_ti[5:4] == 2'b11) begin
+//          acc_ti[5:4] = 2'b11;
+//        end
+//        else begin
+//          acc_ti[5:4] = acc_ti[5:4] + 2'b01;
+//        end
+      end
+      2'b11:begin
+          acc_ti[7:6] = (acc_ti[7:6] == 2'b11) ? 2'b11: (acc_ti[7:6] + 2'b01);
+//        if (acc_ti[7:6] == 2'b11) begin
+//          acc_ti[7:6] = 2'b11;
+//        end
+//        else begin
+//          acc_ti[7:6] = acc_ti[7:6] + 2'b01;
+//        end
+      end
+      default: begin
+        acc_ti[7:0] = 8'b0;
+      end
+    endcase
+  end
+end
+always @ (acc_ti) begin
+  if () begin
+  end
+  else if () begin
+  end
+  else if () begin
+  end
+  else begin
+  end
+end
 endmodule
