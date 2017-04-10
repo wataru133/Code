0605a8a1182a60a7eef720e0180f66016de2f298
Ace <133bha@gmail.com>
2017-04-10 20:38:11 +0700

    Add files via upload

--- dut.v
+++ dut.v
@@ -1,100 +1,54 @@
 //-----------------------------------------------------------------
-// Project Name : Rensas RTL coding training, DesignWS_P1
-// 
-// File Name : dut.v
-// Module Name : lfu
-// Function : finds out the Least Frequently Used entry in the four
-// Author : QuangNguyen
-// Version:0
-//-----------------------------------------------------------------
+//// Project Name : Exercise 1
+//// : lfu
+//// File Name : dut.v
+//// Module Name : lfu
+//// Function : finds out the Least Frequently Used entry in the four.
+//// Note : 
+//// Author : An Bui (HW1906)
+////------------------------------------------------------------------
+//// History
+//// Version Date Author Description
+////---------------------------------------------------------------
+
 module lfu ( clk, rst_n, new_buf_req, ref_buf_numbr, buf_num_replc);
 
-//-----INPUT SIGNALS-----
 input clk;
 input rst_n;
 input new_buf_req;
 input [1:0]ref_buf_numbr;
+
+output [1:0]buf_num_replc;
+
 wire clk;
 wire rst_n;
 wire new_buf_req;
 wire [1:0]ref_buf_numbr;
 
-//-----OUTPUT SIGNALS-----
-output [1:0]buf_num_replc;
 wire [1:0]buf_num_replc;
 
-//-----EXTERNAL SIGNALS-----
-reg [7:0]acc_ti;
-reg [1:0]next_buf_num_replc;
-reg [1:0]f_buf_num_replc;
+reg [1:0]buf_0_cnt;
+reg [1:0]buf_1_cnt;
+reg [1:0]buf_2_cnt;
+reg [1:0]buf_3_cnt;
 
-//-----FF FOR BUF_NUM_REPLC-----
-assign buf_num_replc = f_buf_num_replc;
 always @ (posedge clk or negedge rst_n) begin
-  if (rst_n = 1'b0) begin
-    f_buf_num_repcl <= 2'b00;
-  end
-    f_buf_num_replc <= next_buf_num_replc;
-  else begin
-  end
+    if (rst_n==0) begin
+	    buf_0_cnt = 2'b01;
+        buf_1_cnt = 2'b01;
+        buf_2_cnt = 2'b01;
+        buf_3_cnt = 2'b01;		
+    end
+    else begin
+        buf_0_cnt = buf_0_cnt;
+        buf_1_cnt = buf_1_cnt;
+        buf_2_cnt = buf_2_cnt;
+        buf_3_cnt = buf_3_cnt;
+    end
 end
 
-//-----COUTERN ACCESS TIME-----
-always @ (posedge new_buf_req or ref_buf_numbr) begin
-  if (new_buf_req) begin
-  end
-  else begin
-    case(ref_buf_numbr)
-      2'b00:begin
-          acc_ti[1:0] = (acc_ti[1:0] == 2'b11) ? 2'b11 : (acc_ti[1:0] + 2'b01);
-//        if (acc_ti[1:0] == 2'b11) begin
-//          acc_ti[1:0] = 2'b11;
-//        end
-//        else begin
-//          acc_ti[1:0] = acc_ti[1:0] + 2'b01;
-//        end
-      end
-      2'b01:begin
-          acc_ti[3:2] = (acc_ti[3:2] == 2'b11) ? 2'b11: (acc_ti[3:2] + 2'b01);
-//        if (acc_ti[3:2] == 2'b11) begin
-//          acc_ti[3:2] = 2'b11;
-//        end
-//        else begin
-//          acc_ti[3:2] = acc_ti[3:2] + 2'b01;
-//        end
-      end
-      2'b10:begin
-          acc_ti[5:4] = (acc_ti[5:4] == 2'b11) ? 2'b11: (acc_ti[5:4] + 2'b01);
-//        if (acc_ti[5:4] == 2'b11) begin
-//          acc_ti[5:4] = 2'b11;
-//        end
-//        else begin
-//          acc_ti[5:4] = acc_ti[5:4] + 2'b01;
-//        end
-      end
-      2'b11:begin
-          acc_ti[7:6] = (acc_ti[7:6] == 2'b11) ? 2'b11: (acc_ti[7:6] + 2'b01);
-//        if (acc_ti[7:6] == 2'b11) begin
-//          acc_ti[7:6] = 2'b11;
-//        end
-//        else begin
-//          acc_ti[7:6] = acc_ti[7:6] + 2'b01;
-//        end
-      end
-      default: begin
-        acc_ti[7:0] = 8'b0;
-      end
-    endcase
-  end
-end
-always @ (acc_ti) begin
-  if () begin
-  end
-  else if () begin
-  end
-  else if () begin
-  end
-  else begin
-  end
-end
-endmodule
+
+
+
+
+
